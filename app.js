1cabbb7bc6f6b5f3004f1029becd07fc4f0b4cc8
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-05-12 02:45:24 +0300

    practice done

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 
 const fs = require("fs");
 const axios = require("axios");
-const { log } = require("console");
-const { json } = require("body-parser");
+
 
 //que2
 const readFile = () => {
@@ -57,27 +56,87 @@ const appendToFile = (data) => {
     if (err) throw err;
   });
 };
-appendToFile("ghaidaa");
+ appendToFile("ghaidaa");
 
 //que2
-const copyFile = (fileName)=>{
-    fs.copyFile(`${fileName}`,`copy_of_${fileName}`,(err)=>{
-        if (err) throw err
-        console.log("done")
-    })
-    
-}
-copyFile("data.txt")
+const copyFile = (fileName) => {
+  fs.copyFile(`${fileName}`, `copy_of_${fileName}`, (err) => {
+    if (err) throw err;
+    console.log("done");
+  });
+};
+copyFile("data.txt");
 
 
 //que3
 
-const createPost = (post)=>{
-    axios.post("https://jsonplaceholder.typicode.com/posts")
+const newPost = JSON.stringify({
+  title: "JavaScript Basics",
+  body: "This post contains information about javaScript ",
+  userId: 1,
+});
+
+const createPost = (post) => {
+  axios
+    .post("https://jsonplaceholder.typicode.com/posts", post)
     .then((response) => {
-        console.log(JSON.parse(response.data));
-      })
-      .catch((err) => {
-        throw err;
-      })
-}
+      console.log(response.data);
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+createPost(newPost);
+
+//que4
+
+const newPostt = JSON.stringify({
+  id: 1,
+  title: "Updated Title",
+  body: "Updated body",
+  userId: 1,
+});
+
+const updatePost = (id, data) => {
+  axios
+    .put(`https://jsonplaceholder.typicode.com/todos/${id}`, data)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+updatePost(1, newPostt);
+
+//que5
+const getUsers = async () => {
+  try {
+    let response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log(response);
+  } catch (err) {
+    throw err;
+  }
+};
+getUsers();
+
+//que6
+
+
+
+const saveUsers = async () => {
+  try {
+    let response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    fs.appendFile("users.txt",JSON.stringify(response), (err) => {
+           if (err) throw err;
+       })
+  } catch (err) {
+    throw err;
+  }
+};
+saveUsers()
